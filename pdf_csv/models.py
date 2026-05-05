322de2bc8a0cd035b47b3d5ebe6ff58c9c5e20ea
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-05-05 14:53:26 +0530

    add AjaxStudent model with fields for first name, last name, and email

--- pdf_csv/models.py
+++ pdf_csv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class AjaxStudent(models.Model):
+    first_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
+    email = models.EmailField(unique=True, default='')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
